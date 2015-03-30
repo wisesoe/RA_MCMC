bc37cf8345ae177d5fe4d10255acfc56fcd28890
austinmike <chenxingjing.china@gmail.com>
2015-03-30 22:00:29 +0800

    new code

---
+++ RUN.m
@@ -0,0 +1,41 @@
+
+%[y,x,t]=textread('j.dat');
+
+% y=[3;6;9;12;15;18;21;24;27;30];
+% x=[1 5;2 5;3 5;3.99 5;5 5;6 5;7 5;8 5;9 4;10 5];
+
+
+ x1 = unifrnd(0,10,100,1);
+ x2 = unifrnd(0,10,100,1);
+ e = normrnd(0,2.5,100,1);
+ y = 3*x1+e;
+ x=[x1 x2];
+regress(y,x)
+
+
+a=[0,0];
+m=20000;
+
+[result,I1,I2]=MCMC(a,y,x,m);
+
+%plot
+%                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+subplot(2,2,1)
+hist(result(m/2:end,1),20);
+xlabel('beta1')
+subplot(2,2,2)
+plot(result(:,1))
+subplot(2,2,3)
+hist(result(m/2:end,2),20);
+xlabel('beta2')
+subplot(2,2,4)
+plot(result(:,2))
+
+
+% subplot(3,1,3)
+% hist(result(m/2:end,3),50);
+% xlabel('tau')
+
+
+% 
+%  regress(y,[zeros(length(y),1) x])
